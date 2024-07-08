794e95f3ca7f11e4989bd9a567d1e595523d17d2
ThejanNim <thejannims@gmail.com>
2024-07-08 15:29:20 +0530

    style: format button atom

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -6,7 +6,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button(props: Readonly<ButtonProps>) {
   return (
-    <button className={clsx(props.className, "flex gap-3 w-full justify-center rounded-lg py-[10px] px-4")}>
+    <button
+      {...props}
+      className={clsx(
+        props.className,
+        "flex gap-3 w-full justify-center rounded-lg py-[10px] px-4"
+      )}
+    >
       {props.children}
     </button>
   );
