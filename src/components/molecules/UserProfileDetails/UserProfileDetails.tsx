39c0e65c0f6c970a2dd3c9f02690e32135ce89c8
ThejanNim <thejannims@gmail.com>
2024-07-09 19:15:00 +0530

    feat: implement avatar selection section

---
+++ src/components/molecules/UserProfileDetails/UserProfileDetails.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import Memoji from "@/app/(auth)/signup/onboard/assets/memoji_01.svg";
+
+export function UserProfileDetails() {
+  return (
+    <div className="w-[481px]">
+      <div className="flex flex-col items-center gap-1">
+        <Image
+          src={Memoji}
+          width={100}
+          height={100}
+          alt="profile avatar"
+          className="bg-[#F9F5FF] rounded-[4px]"
+        />
+        <h4 className="text-[#111827] font-medium text-lg">Your name</h4>
+        <div className="py-[2px] px-2 border border-[#EAECF0] rounded-full font-medium text-xs text-[#344054]">
+          Your age
+        </div>
+      </div>
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-3">
+          <div className="flex justify-between">
+            <label htmlFor="type" className="text-[#9CA3AF]">
+              Company Type
+            </label>
+            <div
+              id="type"
+              className="border py-[2px] px-2 rounded-full bg-[#F9FAFB] border-[#EAECF0] text-xs font-medium text-[#344054]"
+            >
+              -
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <label htmlFor="name" className="text-[#9CA3AF]">
+              Company Name
+            </label>
+            <div
+              id="name"
+              className="border py-[2px] px-2 rounded-full bg-[#F9FAFB] border-[#EAECF0] text-xs font-medium text-[#344054]"
+            >
+              -
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <label htmlFor="size" className="text-[#9CA3AF]">
+              Company Size
+            </label>
+            <div
+              id="size"
+              className="border py-[2px] px-2 rounded-full bg-[#F9FAFB] border-[#EAECF0] text-xs font-medium text-[#344054]"
+            >
+              -
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <label htmlFor="role" className="text-[#9CA3AF]">
+              Your role
+            </label>
+            <div
+              id="role"
+              className="border py-[2px] px-2 rounded-full bg-[#F9FAFB] border-[#EAECF0] text-xs font-medium text-[#344054]"
+            >
+              -
+            </div>
+          </div>
+        </div>
+        <p className="text-[#667085] text-center">
+          Powered by <span className="text-[#101828]">onboarding.com</span>
+        </p>
+      </div>
+    </div>
+  );
+}
